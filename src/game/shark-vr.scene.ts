a09f606aadc068a82f315adae3df69a026a8abc6
North McCormick <northmccormick@icloud.com>
2019-01-29 18:39:05 -0800

    feat(input): very rough draft of the input class. This handles mapping the dual-controller to my static steel series, eventually will be customizable

--- src/game/shark-vr.scene.ts
+++ src/game/shark-vr.scene.ts
@@ -4,6 +4,8 @@ import * as GUI from "babylonjs-gui";
 import { Scene } from '../engine/scene.class';
 import { GameUtils } from '../game-utils';
 
+import { Input } from '../engine/input.class';
+
 export class SceneVRShark extends Scene {
 	private _engine: BABYLON.Engine;
 	private _camera: BABYLON.ArcRotateCamera;
@@ -12,8 +14,6 @@ export class SceneVRShark extends Scene {
 	private _sharkMesh: BABYLON.AbstractMesh;
 	private _sharkAnimationTime = 0;
 	private _swim: boolean = false;
-
-	private _gamepadManager: any;
 
 	constructor() {
 		super();
@@ -32,99 +32,7 @@ export class SceneVRShark extends Scene {
 
 		this._camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 30, BABYLON.Vector3.Zero(), this._scene);
 
-
-
-		var gamepadAPI = {
-			controller: {},
-			turbo: false,
-			connect: function(e: any) {
-				gamepadAPI.controller = e.gamepad;
-				gamepadAPI.turbo = true;
-				
-				console.log('Gamepad connected.', e.gamepad);
-			},
-			disconnect: function(e) {
-				gamepadAPI.turbo = false;
-
-				delete gamepadAPI.controller;
-
-				console.log('Gamepad disconnected.');
-			},			
-			update: function() {
-				// clear the buttons cache
-				gamepadAPI.buttonsCache = [];
-				// move the buttons status from the previous frame to the cache
-				for(var k=0; k<gamepadAPI.buttonsStatus.length; k++) {
-					gamepadAPI.buttonsCache[k] = gamepadAPI.buttonsStatus[k];
-				}
-				// clear the buttons status
-				gamepadAPI.buttonsStatus = [];
-				// get the gamepad object
-				var c: any = gamepadAPI.controller || {};
-
-				// loop through buttons and push the pressed ones to the array
-				var pressed = [];
-				// console.log(c);
-				if(c.buttons) {
-					for(var b=0,t=c.buttons.length; b<t; b++) {
-						if(c.buttons[b].pressed || c.buttons[b].value > 0) {
-							console.log('Pressed: ', c.buttons[b], b);
-							pressed.push(gamepadAPI.buttons[b]);
-						}
-					}
-				}
-
-				// loop through axes and push their values to the array
-				var axes = [];
-				if(c.axes) {
-					for(var a=0,x=c.axes.length; a<x; a++) {
-						axes.push(c.axes[a].toFixed(2));
-					}
-				}
-				// assign received values
-				gamepadAPI.axesStatus = axes;
-				gamepadAPI.buttonsStatus = pressed;
-				// return buttons for debugging purposes
-				return pressed;
-			},
-			buttonPressed: function(button, hold) {
-				var newPress = false;
-				// loop through pressed buttons
-				for(var i=0,s=gamepadAPI.buttonsStatus.length; i<s; i++) {
-					// if we found the button we're looking for...
-					if(gamepadAPI.buttonsStatus[i] == button) {
-						// set the boolean variable to true
-						newPress = true;
-						// if we want to check the single press
-						if(!hold) {
-							// loop through the cached states from the previous frame
-							for(var j=0,p=gamepadAPI.buttonsCache.length; j<p; j++) {
-								// if the button was already pressed, ignore new press
-								if(gamepadAPI.buttonsCache[j] == button) {
-									newPress = false;
-								}
-							}
-						}
-					}
-				}
-				return newPress;
-			},
-			buttons: [
-				'DPad-Up','DPad-Down','DPad-Left','DPad-Right',
-				'Start','Back','Axis-Left','Axis-Right',
-				'LB','RB','Power','A','B','X','Y',
-			],
-			buttonsCache: [],
-			buttonsStatus: [],
-			axesStatus: []
-		};
-
-		window.addEventListener("gamepadconnected", gamepadAPI.connect);
-		window.addEventListener("gamepaddisconnected", gamepadAPI.disconnect);
-
-
-		this._gamepadManager = gamepadAPI;
-
+		Input.getInstance().init();
 
 		// this._camera.attachControl(this._canvas, true);
 
@@ -160,7 +68,7 @@ export class SceneVRShark extends Scene {
 			console.log(gamepad, state);
 		});*/
 
-		this._light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
+		/*this._light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
 
 		let skybox = GameUtils.createSkybox("skybox", "./assets/texture/skybox/TropicalSunnyDay", this._scene);
 
@@ -192,21 +100,31 @@ export class SceneVRShark extends Scene {
 				else {
 					textControl.text = "Start Swimming";
 				}
-			});
+			});*/
 	}
 
 	sceneBeforeRender(deltaTime) {
 		super.sceneBeforeRender(deltaTime);
 
-		this._gamepadManager.update();
+		Input.getInstance().update();
 
-		if (this._gamepadManager.buttonPressed('A')) {
+		if (Input.getInstance().buttonPressed('A', false)) {
+			console.log('A!');
+		}
+
+		if (Input.getInstance().buttonPressed('A', true)) {
+			console.log('AAA!');
+		}
+
+		// this._gamepadManager.update();
+
+		/*if (this._gamepadManager.buttonPressed('A')) {
 			console.log('a');
 
 			this._swim = !this._swim;
-		}
+		}*/
 
-		this.animateShark(deltaTime);
+		// this.animateShark(deltaTime);
 	}
 
 	animateShark(deltaTime: number): void {
